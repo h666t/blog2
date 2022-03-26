9dd00de2f5d4b20a22c21c999b6b8bc45ed26594
h666t <1845720928@qq.com>
2022-03-26 18:24:16 +0800

    update

--- src/migration/addCreateAtAndUpdateAt.ts
+++ src/migration/addCreateAtAndUpdateAt.ts
@@ -5,31 +5,31 @@ export class addCreateAtAndUpdateAt1648289313726 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumns('blogs', [
-      new TableColumn({name: 'createAt', type: 'time', default: 'now()'}),
-      new TableColumn({name: 'updateAt', type: 'time', default: 'now()'}),
+      new TableColumn({name: 'create_at', type: 'time', default: 'now()'}),
+      new TableColumn({name: 'update_at', type: 'time', default: 'now()'}),
     ]);
     await queryRunner.addColumns('comments', [
-      new TableColumn({name: 'createAt', type: 'time', default: 'now()'}),
-      new TableColumn({name: 'updateAt', type: 'time', default: 'now()'}),
+      new TableColumn({name: 'create_at', type: 'time', default: 'now()'}),
+      new TableColumn({name: 'update_at', type: 'time', default: 'now()'}),
     ]);
     await queryRunner.addColumns('users', [
-      new TableColumn({name: 'createAt', type: 'time', default: 'now()'}),
-      new TableColumn({name: 'updateAt', type: 'time', default: 'now()'}),
+      new TableColumn({name: 'create_at', type: 'time', default: 'now()'}),
+      new TableColumn({name: 'update_at', type: 'time', default: 'now()'}),
     ]);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropColumns('blogs', [
-      new TableColumn({name: 'createAt', type: 'time', default: 'now()'}),
-      new TableColumn({name: 'updateAt', type: 'time', default: 'now()'}),
+      new TableColumn({name: 'create_at', type: 'time', default: 'now()'}),
+      new TableColumn({name: 'update_at', type: 'time', default: 'now()'}),
     ]);
     await queryRunner.dropColumns('comments', [
-      new TableColumn({name: 'createAt', type: 'time', default: 'now()'}),
-      new TableColumn({name: 'updateAt', type: 'time', default: 'now()'}),
+      new TableColumn({name: 'create_at', type: 'time', default: 'now()'}),
+      new TableColumn({name: 'update_at', type: 'time', default: 'now()'}),
     ]);
     await queryRunner.dropColumns('users', [
-      new TableColumn({name: 'createAt', type: 'time', default: 'now()'}),
-      new TableColumn({name: 'updateAt', type: 'time', default: 'now()'}),
+      new TableColumn({name: 'create_at', type: 'time', default: 'now()'}),
+      new TableColumn({name: 'update_at', type: 'time', default: 'now()'}),
     ]);
   }
 
